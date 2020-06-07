76b46e5e0e28987156f3922c580fd30464e5c0b9
Fatih Kucuk <1fatihkucuk@gmail.com>
2020-06-08 01:23:21 +0300

    Remove comment and change hashrouter to run in gh pages

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
-import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
+import { Switch, Route, HashRouter, Redirect } from "react-router-dom";
 import routes from "./routes";
 import Container from "react-bootstrap/Container";
 
 function App() {
-  // const defaultLinks = [
-  //   {
-  //     id: 1,
-  //     name: "Hacker News",
-  //     url: "https://news.ycombinator.com/",
-  //     points: 3,
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Product Hunt",
-  //     url: "https://producthunt.com/",
-  //     points: 6,
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "REDDIT",
-  //     url: "https://www.reddit.com/",
-  //     points: 3,
-  //   },
-  // ];
-  // try {
-  //   const links = JSON.parse(localStorage.getItem("links")) || [];
-  //   links.push(defaultLinks);
-  //   const stringifiedLinks = JSON.stringify(defaultLinks);
-  //   localStorage.setItem("links", stringifiedLinks);
-  // } catch (err) {
-  //   alert(err);
-  // }
-
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Container fluid>
         <Header />
         <Switch>
@@ -46,7 +17,7 @@ function App() {
           })}
         </Switch>
       </Container>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
